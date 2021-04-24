84f22fa308e4d97955106b93127e5ef34746fbdc
leofs88 <leonardo.soares@fpf.br>
2021-04-24 19:02:06 -0400

    Initial Commit

---
+++ NewsInTime/NewsInTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsInTimeApp.swift
+//  NewsInTime
+//
+//  Created by Leonardo Soares on 24/04/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsInTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
